cbd45a9041d437ce604354fce58ef4db98be6358
ESWhite07 <ESWhite@anderson.edu>
2025-02-12 23:51:19 -0500

    Update git_project.py

--- git_project.py
+++ git_project.py
@@ -2,3 +2,5 @@ hello_world = " Hello World"
 #Putting hello world into a variable
 print (hello_world)
 #printing an output of the variable that was made before.
+
+#Next we will seperate every word on a different line
